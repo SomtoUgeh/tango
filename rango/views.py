e65cf7e032155d8cfd0ca4649bc20d939e56ee5f
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2021-07-27 21:23:08 +0100

    finish exercise in CH6

--- rango/views.py
+++ rango/views.py
@@ -6,10 +6,12 @@ from rango.models import Page
 # Create your views here.
 def index(request):
   category_list = Category.objects.order_by('-likes')[:5]
+  pages_list = Page.objects.order_by('-views')[:5]
 
   context_dict = {}
   context_dict['boldmessages'] = "Crunchy, creamy, cookie, candy, cupcake!"
   context_dict['categories'] = category_list
+  context_dict['pages'] = pages_list
 
   return render(request, 'rango/index.html', context=context_dict)
 
