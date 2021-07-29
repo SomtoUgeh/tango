1bc6cc0462b107ef1794420adbca91ada628cfb4
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2021-07-29 10:30:35 +0100

    add logout functionality

--- rango/views.py
+++ rango/views.py
@@ -160,3 +160,13 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
