2bb5f4b5dd435d288048dbffd0afef3b14a2bccd
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2021-07-29 10:09:05 +0100

    finish up user registration flow

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, UserForm, UserProfileForm
 from rango.forms import PageForm
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -103,3 +103,33 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+      context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
